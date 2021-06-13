cc147bb8efc045b46db7ff35d058cf05818eb5e1
HassanVeriheal <85360795+talha-rafiq@users.noreply.github.com>
2021-06-13 23:20:58 +0500

    post-submittion

--- doctor-panel/layout/sidebar.php
+++ doctor-panel/layout/sidebar.php
@@ -63,12 +63,6 @@
                     <span>Schedule</span>
                 </a>
             </li>
-            <li class="<?php active_page('posts'); ?>">
-                <a href="<?= admin_base_url();?>Posts">
-                    <i class="fa fa-clock-o"></i>
-                    <span>Posts</span>
-                </a>
-            </li>
             <?php
             $sql_new = query("SELECT * FROM tbl_membership WHERE membership_id = ". get_sess('userdata')['doc_membership']);
             $fetch = fetch($sql_new);
