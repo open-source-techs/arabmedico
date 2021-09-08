eaebd955d6f5755333c517c529af9021cbba06cf
HassanVeriheal <85360795+talha-rafiq@users.noreply.github.com>
2021-09-08 18:06:40 +0500

    feature-doctor-and-clinic-management-update

--- admin/feature-doctor.php
+++ admin/feature-doctor.php
@@ -21,34 +21,59 @@
                 <div class="panel panel-bd lobidrag">
                     <div class="panel-heading"></div>
                     <div class="panel-body">
-                        <form  action="<?= admin_base_url()?>model/featureModel" method="POST" enctype="multipart/form-data" class="col-sm-12">
-                            <div class="col-sm-6 form-group">
-                                <label>Select Doctor</label>
-                                <select name="txt_doctor"  id="doctor" class="form-control select2" required>
-                                    <option>Select Doctor</option>
-                                    <?php
-                                    $sql = query("SELECT * FROM tbl_doctor WHERE doc_active = 1");
-                                    while ($row = fetch($sql))
-                                    {
-                                        ?>
-                                        <option value="<?= $row['doc_id'];?>"><?= $row['doc_name'];?></option>
+                        <form  action="<?= admin_base_url()?>model/featureModel" method="POST" enctype="multipart/form-data">
+                            <div class="col-md-12">
+                                <div class="col-sm-6 form-group">
+                                    <label>Select Doctor</label>
+                                    <select name="txt_doctor"  id="doctor" class="form-control select2" required>
+                                        <option>Select Doctor</option>
                                         <?php
-                                    }
-                                    ?>
-                                </select>
+                                        $sql = query("SELECT * FROM tbl_doctor WHERE doc_active = 1");
+                                        while ($row = fetch($sql))
+                                        {
+                                            ?>
+                                            <option value="<?= $row['doc_id'];?>"><?= $row['doc_name'];?></option>
+                                            <?php
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <label>Start Date</label>
+                                    <input type="date" name="txt_startDate" class="form-control">
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <label>Select Tenure</label>
+                                    <select name="txt_tenure" class="form-control select2" required>
+                                        <option value="quater">3 Months</option>
+    									<option value="half">6 Months</option>
+    									<option value="full">1 Year</option>
+                                        <option value="fix">Permanent</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <label>Enable Feature for Home Page</label>
+                                    <select name="txt_homefeature" id="txt_homefeature" class="form-control" required>
+                                        <option value="no">NO</option>
+                                        <option value="yes">YES</option>
+                                    </select>
+                                </div>
                             </div>
-                            <div class="col-sm-6 form-group">
-                                <label>Start Date</label>
-                                <input type="date" name="txt_startDate" class="form-control">
-                            </div>
-                            <div class="col-sm-6 form-group">
-                                <label>Select Tenure</label>
-                                <select name="txt_tenure" class="form-control select2" required>
-                                    <option value="quater">3 Months</option>
-									<option value="half">6 Months</option>
-									<option value="full">1 Year</option>
-                                    <option value="fix">Permanent</option>
-                                </select>
+                            <div class="col-md-12" id="home_div" style="display:none">
+                                <div class="col-sm-6 form-group">
+                                    <label>Start Date</label>
+                                    <input type="date" name="txt_homeStartDate" id="txt_homeStartDate" class="form-control">
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <label>Select Tenure</label>
+                                    <select name="txt_homeTenure" id="txt_homeTenure" class="form-control">
+                                        <option value="">Select One</option>
+                                        <option value="quater">3 Months</option>
+                                        <option value="half">6 Months</option>
+                                        <option value="full">1 Year</option>
+                                        <option value="fix">Permanent</option>
+                                    </select>
+                                </div>
                             </div>
                             <div class="col-sm-12 reset-button">
                                 <input type="submit" name="btn_save_doctor" class="btn btn-success" value="Save">
@@ -79,9 +104,11 @@
                                         <th>Image</th>
                                         <th>Name</th>
                                         <th>Email</th>
+                                        <th>List Tenure</th>
                                         <th>Start Date</th>
-                                        <th>Tenure</th>
-                                        <th>End Date</th>
+                                        <th>Landing Feature</th>
+                                        <th>Home Tenure</th>
+                                        <th>Start Date</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
@@ -95,9 +122,11 @@
                                             <td><img style="width:auto;height:50px" src="<?= file_url().$row['doc_image'];?>" > </td>
                                             <td><?= $row['doc_name'];?></td>
                                             <td><?= $row['doc_email'];?></td>
-                                            <td><?= date("d/m/Y",strtotime($row['f_start_date']));?></td>
                                             <td><?= ($row['f_tenure'] == "quater") ? '3 Months' : (($row['f_tenure'] == "half") ? '6 Months' : (($row['f_tenure'] == "full") ? '1 Year' : 'Permanent' )); ?> </td>
-                                            <td><?= date("d/m/Y",strtotime($row['f_end_date']));?></td>
+                                            <td><?= date("d/m/Y",strtotime($row['f_start_date']));?></td>
+                                            <td><?= strtoupper($row['f_home']);?></td>
+                                            <td><?= ($row['f_home_tenure'] == "quater") ? '3 Months' : (($row['f_home_tenure'] == "half") ? '6 Months' : (($row['f_home_tenure'] == "full") ? '1 Year' : (($row['f_home_tenure'] == "fix") ? 'Permanent' : 'N/A') )); ?> </td>
+                                            <td><?= ($row['f_home_start'] != null) ? date("d/m/Y",strtotime($row['f_home_start'])) : 'N/A';?></td>
                                             <td>
                                                 <a href="<?= admin_base_url();?>model/featureModel?act=del-doc&f_id=<?= $row['f_id']; ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i>
                                                 </a>
@@ -124,5 +153,17 @@ get_msg('msg');
 <script type="text/javascript">
     $(document).ready(function(){
         $(".select2").select2();
+        $("#txt_homefeature").change(function(){
+            if($(this).val() == "yes")
+            {
+                $("#home_div").show();
+            }
+            else if($(this).val() == "no")
+            {
+                $("#home_div").hide();
+                $("#txt_homeStartDate").val("");
+                $("#txt_homeTenure").val("");
+            }
+        });
     });
 </script>
--- admin/model/featureModel.php
+++ admin/model/featureModel.php
@@ -5,91 +5,161 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 	if(isset($_POST['btn_save_doctor']))
 	{
 		$data['f_doctor_id'] 	= post('txt_doctor');
-		$data['f_tenure'] 		= post('txt_tenure');
-		$data['f_start_date'] 	= post('txt_startDate');
-		if($data['f_tenure'] == 'quater')
+		$checkSql 				= query("SELECT * FROM tbl_feature_doctor WHERE f_doctor_id = ".$data['f_doctor_id']." AND f_active = 1");
+		if(nrows($checkSql) > 0)
 		{
-			$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +3 Months"));
-		}
-		else if($data['f_tenure'] == 'half')
-		{
-			$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +6 Months"));
-		}
-		else if($data['f_tenure'] == 'full')
-		{
-			$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +12 Months"));
-		}
-		else if($data['f_tenure'] == 'fix')
-		{
-			$data['f_end_date'] = 'fix';
-		}
-		if(
-			$data['f_doctor_id'] 	!= "" && $data['f_doctor_id'] 	!= null &&
-			$data['f_tenure'] 		!= "" && $data['f_tenure'] 		!= null &&
-			$data['f_start_date'] 	!= "" && $data['f_start_date'] 	!= null &&
-			$data['f_end_date'] 	!= "" && $data['f_end_date'] 	!= null
-		)
-		{
-			if(insert2($data,'tbl_feature_doctor'))
-			{
-				set_msg('Success','Feature Doctor is added successfully','success');
-			    jump(admin_base_url()."feature-doctor");
-			}
-			else
-			{
-				set_msg('Insertion error','Unable to process your request. Please try again later.','error');
-				echo "<script>window.history.go(-1);</script>";
-			}
+			set_msg('Duplicate error','This doctor is already in feature list.','error');
+			echo "<script>window.history.go(-1);</script>";
 		}
 		else
 		{
-			set_msg('Fields Error','Please fill all fields with data','error');
-			echo "<script>window.history.go(-1);</script>";
+			$data['f_tenure'] 		= post('txt_tenure');
+			$data['f_start_date'] 	= post('txt_startDate');
+			if($data['f_tenure'] == 'quater')
+			{
+				$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +3 Months"));
+			}
+			else if($data['f_tenure'] == 'half')
+			{
+				$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +6 Months"));
+			}
+			else if($data['f_tenure'] == 'full')
+			{
+				$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +12 Months"));
+			}
+			else if($data['f_tenure'] == 'fix')
+			{
+				$data['f_end_date'] = 'fix';
+			}
+			$data['f_home'] 	= post('txt_homefeature');
+			if($data['f_home'] == "yes")
+			{
+				$data['f_home_start'] 	= post('txt_homeStartDate');
+				$data['f_home_tenure'] 	= post('txt_homeTenure');
+				if($data['f_home_tenure'] == '' || $data['f_home_tenure'] == null)
+				{
+					$data['f_home_tenure'] = 'quater';
+				}
+				if($data['f_home_tenure'] == 'quater')
+				{
+					$data['f_home_end	'] = date("Y-m-d", strtotime($data['f_home_start'] . " +3 Months"));
+				}
+				else if($data['f_home_tenure'] == 'half')
+				{
+					$data['f_home_end	'] = date("Y-m-d", strtotime($data['f_home_start'] . " +6 Months"));
+				}
+				else if($data['f_home_tenure'] == 'full')
+				{
+					$data['f_home_end	'] = date("Y-m-d", strtotime($data['f_home_start'] . " +12 Months"));
+				}
+				else if($data['f_home_tenure'] == 'fix')
+				{
+					$data['f_home_end	'] = 'fix';
+				}
+			}
+			if(
+				$data['f_doctor_id'] 	!= "" && $data['f_doctor_id'] 	!= null &&
+				$data['f_tenure'] 		!= "" && $data['f_tenure'] 		!= null &&
+				$data['f_start_date'] 	!= "" && $data['f_start_date'] 	!= null &&
+				$data['f_end_date'] 	!= "" && $data['f_end_date'] 	!= null
+			)
+			{
+				if(insert2($data,'tbl_feature_doctor'))
+				{
+					set_msg('Success','Feature Doctor is added successfully','success');
+				    jump(admin_base_url()."feature-doctor");
+				}
+				else
+				{
+					set_msg('Insertion error','Unable to process your request. Please try again later.','error');
+					echo "<script>window.history.go(-1);</script>";
+				}
+			}
+			else
+			{
+				set_msg('Fields Error','Please fill all fields with data','error');
+				echo "<script>window.history.go(-1);</script>";
+			}
 		}
 	}
 	else if(isset($_POST['btn_save_clinic']))
 	{
 		$data['f_clinic_id'] 	= post('txt_clinic');
-		$data['f_tenure'] 		= post('txt_tenure');
-		$data['f_start_date'] 	= post('txt_startDate');
-		if($data['f_tenure'] == 'quater')
+		$checkSql 				= query("SELECT * FROM tbl_feature_clinic WHERE f_clinic_id = ".$data['f_clinic_id']." AND f_active = 1");
+		if(nrows($checkSql) > 0)
 		{
-			$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +3 Months"));
-		}
-		else if($data['f_tenure'] == 'half')
-		{
-			$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +6 Months"));
-		}
-		else if($data['f_tenure'] == 'full')
-		{
-			$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +12 Months"));
-		}
-		else if($data['f_tenure'] == 'fix')
-		{
-			$data['f_end_date'] = 'fix';
-		}
-		if(
-			$data['f_clinic_id'] 	!= "" && $data['f_clinic_id'] 	!= null &&
-			$data['f_tenure'] 		!= "" && $data['f_tenure'] 		!= null &&
-			$data['f_start_date'] 	!= "" && $data['f_start_date'] 	!= null &&
-			$data['f_end_date'] 	!= "" && $data['f_end_date'] 	!= null
-		)
-		{
-			if(insert2($data,'tbl_feature_clinic'))
-			{
-				set_msg('Success','Feature Clinic is added successfully','success');
-			    jump(admin_base_url()."feature-clinic");
-			}
-			else
-			{
-				set_msg('Insertion error','Unable to process your request. Please try again later.','error');
-				echo "<script>window.history.go(-1);</script>";
-			}
+			set_msg('Duplicate error','This Clinic is already in feature list.','error');
+			echo "<script>window.history.go(-1);</script>";
 		}
 		else
 		{
-			set_msg('Fields Error','Please fill all fields with data','error');
-			echo "<script>window.history.go(-1);</script>";
+			$data['f_tenure'] 		= post('txt_tenure');
+			$data['f_start_date'] 	= post('txt_startDate');
+			if($data['f_tenure'] == 'quater')
+			{
+				$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +3 Months"));
+			}
+			else if($data['f_tenure'] == 'half')
+			{
+				$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +6 Months"));
+			}
+			else if($data['f_tenure'] == 'full')
+			{
+				$data['f_end_date'] = date("Y-m-d", strtotime($data['f_start_date'] . " +12 Months"));
+			}
+			else if($data['f_tenure'] == 'fix')
+			{
+				$data['f_end_date'] = 'fix';
+			}
+			$data['f_home'] 	= post('txt_homefeature');
+			if($data['f_home'] == "yes")
+			{
+				$data['f_home_start'] 	= post('txt_homeStartDate');
+				$data['f_home_tenure'] 	= post('txt_homeTenure');
+				if($data['f_home_tenure'] == '' || $data['f_home_tenure'] == null)
+				{
+					$data['f_home_tenure'] = 'quater';
+				}
+				if($data['f_home_tenure'] == 'quater')
+				{
+					$data['f_home_end	'] = date("Y-m-d", strtotime($data['f_home_start'] . " +3 Months"));
+				}
+				else if($data['f_home_tenure'] == 'half')
+				{
+					$data['f_home_end	'] = date("Y-m-d", strtotime($data['f_home_start'] . " +6 Months"));
+				}
+				else if($data['f_home_tenure'] == 'full')
+				{
+					$data['f_home_end	'] = date("Y-m-d", strtotime($data['f_home_start'] . " +12 Months"));
+				}
+				else if($data['f_home_tenure'] == 'fix')
+				{
+					$data['f_home_end	'] = 'fix';
+				}
+			}
+			if(
+				$data['f_clinic_id'] 	!= "" && $data['f_clinic_id'] 	!= null &&
+				$data['f_tenure'] 		!= "" && $data['f_tenure'] 		!= null &&
+				$data['f_start_date'] 	!= "" && $data['f_start_date'] 	!= null &&
+				$data['f_end_date'] 	!= "" && $data['f_end_date'] 	!= null
+			)
+			{
+				if(insert2($data,'tbl_feature_clinic'))
+				{
+					set_msg('Success','Feature Clinic is added successfully','success');
+				    jump(admin_base_url()."feature-clinic");
+				}
+				else
+				{
+					set_msg('Insertion error','Unable to process your request. Please try again later.','error');
+					echo "<script>window.history.go(-1);</script>";
+				}
+			}
+			else
+			{
+				set_msg('Fields Error','Please fill all fields with data','error');
+				echo "<script>window.history.go(-1);</script>";
+			}
 		}
 	}
 	else
